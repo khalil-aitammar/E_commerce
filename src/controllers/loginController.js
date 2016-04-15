b2d81169251506fbde0df0a1ac6831edde90e62b
khalil ait ammar <ait.ammar.khalil@gmail.com>
2016-04-15 22:10:40 +0200

    correction du login erreur

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,35 +1,33 @@
-app.controller('loginController', function($scope, $http) {
+app.controller('loginController', function ($scope, $http) {
     $scope.login = "";
     $scope.mp = "";
+    $scope.sub = function () {
+        //  $location.path('/accueil');
+        $http({
+            method: 'GET',
+            url: './js/data.js'
+        }).then(function successCallback(response) {
 
-    $scope.sub=function(){
+            for (var i = 0; i < response.data.length; i++) {
+                console.log($scope.login, $scope.mp);
+                if (response.data[i].name == $scope.login && response.data[i].mp == $scope.mp) {
 
-            //  $location.path('/accueil');
+                    $scope.formup = true;
 
-
-            $http({
-                method: 'GET',
-                url: './js/data.js'
-            }).then(function successCallback(response) {
-
-                for (var i=0;i < response.data.length;i++ ){
-                    console.log($scope.login,$scope.mp);
-                    if(response.data[i].name == $scope.login && response.data[i].mp == $scope.mp){
-
-                        $scope.formup=true;
-
-                    }  else {
-                        alert('login incorrect');
-
-                    }
                 }
+            }
 
-            }, function errorCallback(response) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-            });
-          
-
+        }).then(function () {
+            if ($scope.formup==false){
+                alert("erreur");
+            }
+        })
     };
+
+
+
+
+
+
 });
 
