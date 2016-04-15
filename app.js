d26b70a79337fae0fa20c26e8f8f09e14310ff79
khalil ait ammar <ait.ammar.khalil@gmail.com>
2016-04-15 02:19:03 +0200

    ajout de commentaires sur les routes

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 'use strict';
 var app = angular.module('ecom', ['ngRoute'] );
+
+//configuration de routeProvider 
 app .config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
+        // url page film
         when('/film', {
             templateUrl: 'src/Views/film.html',
 
         }).
-        when('/seris', {
-            templateUrl: 'src/Views/seris.html',
+        // url page series
+        when('/series', {
+            templateUrl: 'src/Views/series.html',
 
         }).
+            // url par defaut
         otherwise({
             redirectTo: '/film'
         });
