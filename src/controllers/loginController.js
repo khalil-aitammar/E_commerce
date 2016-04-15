440ef538c16d3d3167498655205a6d3d5e74d844
khalil ait ammar <ait.ammar.khalil@gmail.com>
2016-04-15 18:13:28 +0200

    ajout de bloc login succes  html & css

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,10 +1,11 @@
 app.controller('loginController', function($scope) {
     $scope.login = "";
     $scope.mp = "";
-    $scope.sub=function(){
 
+    $scope.sub=function(){
         if($scope.login=='khalil'&& $scope.mp=='ait'){
             //  $location.path('/accueil');
+            $scope.formup=true;
             alert('login zabour');
         }else {
             alert('login incorrect');
