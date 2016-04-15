9ffee4cbbafad3340b9a15cf701c2e70b8f0f54b
khalil ait ammar <ait.ammar.khalil@gmail.com>
2016-04-15 20:47:03 +0200

    ajout de la verification du login&mp a partire d'un fichier json avec le servier $http

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,14 +1,35 @@
-app.controller('loginController', function($scope) {
+app.controller('loginController', function($scope, $http) {
     $scope.login = "";
     $scope.mp = "";
 
     $scope.sub=function(){
-        if($scope.login=='khalil'&& $scope.mp=='ait'){
+
             //  $location.path('/accueil');
-            $scope.formup=true;
-            alert('login zabour');
-        }else {
-            alert('login incorrect');
-        }
+
+
+            $http({
+                method: 'GET',
+                url: './js/data.js'
+            }).then(function successCallback(response) {
+
+                for (var i=0;i < response.data.length;i++ ){
+                    console.log($scope.login,$scope.mp);
+                    if(response.data[i].name == $scope.login && response.data[i].mp == $scope.mp){
+
+                        $scope.formup=true;
+
+                    }  else {
+                        alert('login incorrect');
+
+                    }
+                }
+
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+          
+
     };
 });
+
